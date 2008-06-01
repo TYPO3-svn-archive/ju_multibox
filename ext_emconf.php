d4da8ba16a59743aed2ed7db970b77c82c1ecb7e
uhlmann <uhlmann@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-01 20:42:21 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ju_multibox/trunk@9809 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "ju_multibox"
+#
+# Auto generated 01-06-2008 22:39
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Multibox',
+	'description' => 'Multibox 1.3.1: lightbox that supports images, flash, video, mp3s, html. Added some configuration features.',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '0.0.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Jonathan Uhlmann',
+	'author_email' => 'uhlmann@augenreiz.ch',
+	'author_company' => 'www.augenreiz.ch',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"b0c7";s:10:"README.txt";s:4:"ee2d";s:23:"class.tx_jumultibox.php";s:4:"3078";s:12:"ext_icon.gif";s:4:"e135";s:14:"ext_tables.php";s:4:"5ac9";s:13:"locallang.xml";s:4:"28b8";s:15:"res/css/ie6.css";s:4:"8cde";s:20:"res/css/iepngfix.htc";s:4:"169b";s:20:"res/css/multibox.css";s:4:"2579";s:24:"res/css/images/close.png";s:4:"b7fd";s:23:"res/css/images/left.png";s:4:"7028";s:31:"res/css/images/leftDisabled.png";s:4:"6527";s:25:"res/css/images/loader.gif";s:4:"b256";s:24:"res/css/images/right.png";s:4:"838f";s:32:"res/css/images/rightDisabled.png";s:4:"7252";s:18:"res/js/mootools.js";s:4:"17ff";s:18:"res/js/multibox.js";s:4:"7c51";s:17:"res/js/overlay.js";s:4:"52f5";s:33:"res/player/AC_RunActiveContent.js";s:4:"3e03";s:28:"res/player/Corona_Skin_1.swf";s:4:"7398";s:28:"res/player/Corona_Skin_2.swf";s:4:"d61a";s:36:"res/player/FLVPlayer_Progressive.swf";s:4:"3099";s:24:"res/player/flvplayer.fla";s:4:"db6f";s:24:"res/player/flvplayer.swf";s:4:"da0a";s:24:"res/player/mp3player.fla";s:4:"d82f";s:24:"res/player/mp3player.swf";s:4:"2849";s:29:"static/multibox/constants.txt";s:4:"2ac1";s:25:"static/multibox/setup.txt";s:4:"6fac";}',
+);
+
+?>
